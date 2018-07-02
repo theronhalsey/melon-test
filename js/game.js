3f47694c8d94d95992c9666d943c366ebd86a8d6
Theron Halsey <theron.halsey@gmail.com>
2018-07-02 17:34:47 -0400

    replace old stuff in proper repo now... hopefully

--- js/game.js
+++ js/game.js
@@ -1,39 +1,83 @@
-
-/* Game namespace */
+/**
+ * main
+ */
 var game = {
 
-    // an object where to store game information
+    /**
+     * object where to store game global data
+     */
     data : {
         // score
         score : 0
     },
 
+    /**
+     *
+     * Initialize the application
+     */
+    onload: function() {
 
-    // Run on page load.
-    "onload" : function () {
-        // Initialize the video.
-        if (!me.video.init(960, 640, {wrapper : "screen", scale : "auto"})) {
+        // init the video
+        if (!me.video.init(800, 600, {wrapper : "screen", scale : "auto", scaleMethod : "flex-width", renderer : me.video.CANVAS, subPixel : false })) {
             alert("Your browser does not support HTML5 canvas.");
             return;
         }
 
-        // Initialize the audio.
+        // initialize the "sound engine"
         me.audio.init("mp3,ogg");
 
-        // set and load all resources.
-        // (this will also automatically switch to the loading screen)
+        // set all ressources to be loaded
         me.loader.preload(game.resources, this.loaded.bind(this));
     },
 
-    // Run on game resources loaded.
-    "loaded" : function () {
-        me.state.set(me.state.MENU, new game.TitleScreen());
+
+    /**
+     * callback when everything is loaded
+     */
+    loaded: function ()    {
+
+        // set the "Play/Ingame" Screen Object
         me.state.set(me.state.PLAY, new game.PlayScreen());
 
-        // add our player entity in the entity pool
-        me.pool.register("mainPlayer", game.PlayerEntity);
+        // set the fade transition effect
+        me.state.transition("fade", "#FFFFFF", 250);
 
-        // Start the game.
+        // register our objects entity in the object pool
+        me.pool.register("mainPlayer", game.PlayerEntity);
+        me.pool.register("SlimeEntity", game.SlimeEnemyEntity);
+        me.pool.register("FlyEntity", game.FlyEnemyEntity);
+        me.pool.register("CoinEntity", game.CoinEntity);
+
+        // load the texture atlas file
+        // this will be used by object entities later
+        game.texture = new me.video.renderer.Texture(
+            me.loader.getJSON("texture"),
+            me.loader.getImage("texture")
+        );
+
+        // add some keyboard shortcuts
+        me.event.subscribe(me.event.KEYDOWN, function (action, keyCode /*, edge */) {
+
+            // change global volume setting
+            if (keyCode === me.input.KEY.PLUS) {
+                // increase volume
+                me.audio.setVolume(me.audio.getVolume()+0.1);
+            } else if (keyCode === me.input.KEY.MINUS) {
+                // decrease volume
+                me.audio.setVolume(me.audio.getVolume()-0.1);
+            }
+
+            // toggle fullscreen on/off
+            if (keyCode === me.input.KEY.F) {
+                if (!me.device.isFullscreen) {
+                    me.device.requestFullscreen();
+                } else {
+                    me.device.exitFullscreen();
+                }
+            }
+        });
+
+        // switch to PLAY state
         me.state.change(me.state.PLAY);
     }
 };
--- js/screens/play.js
+++ js/screens/play.js
@@ -3,20 +3,44 @@ game.PlayScreen = me.ScreenObject.extend({
      *  action to perform on state change
      */
     onResetEvent: function() {
+      // load a level
+        me.levelDirector.loadLevel("map1");
+
         // reset the score
         game.data.score = 0;
 
-        // Add our HUD to the game world, add it last so that this is on top of the rest.
-        // Can also be forced by specifying a "Infinity" z value to the addChild function.
-        this.HUD = new game.HUD.Container();
+        // add our HUD to the game world
+        if (typeof this.HUD === "undefined") {
+            this.HUD = new game.HUD.UIContainer();
+        }
         me.game.world.addChild(this.HUD);
+
+        // display if debugPanel is enabled or on mobile
+        if ((me.plugins.debugPanel && me.plugins.debugPanel.panel.visible) || me.device.touch) {
+            if (typeof this.virtualJoypad === "undefined") {
+                this.virtualJoypad = new game.HUD.VirtualJoypad();
+            }
+            me.game.world.addChild(this.virtualJoypad);
+        }
+
+        // play some music
+        me.audio.playTrack("dst-gameforest");
     },
 
     /**
-     *  action to perform when leaving this screen (state change)
+     *  action to perform on state change
      */
     onDestroyEvent: function() {
+
         // remove the HUD from the game world
         me.game.world.removeChild(this.HUD);
+
+        // remove the joypad if initially added
+        if (this.virtualJoypad && me.game.world.hasChild(this.virtualJoypad)) {
+            me.game.world.removeChild(this.virtualJoypad);
+        }
+
+        // stop some music
+        me.audio.stopTrack("dst-gameforest");
     }
 });
